1b542aec6d02fcb68adc362f7d1fc9e45ce67396
Hanna Malkova <hanna_malkova@epam.com>
2020-11-13 10:31:22 +0200

    Task1: Core concept. Final.

--- app1/src/index.js
+++ app1/src/index.js
@@ -3,36 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Elements from './task1';
-import Comp from './task2';
-import PureComp from './task3';
-import Func from './task4';
+
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
-
-ReactDOM.render(
-  Elements,
-  document.getElementById('task1')
-);
-
-ReactDOM.render(
-  <Comp />,
-  document.getElementById('task2')
-);
-
-ReactDOM.render(
-  <PureComp />,
-  document.getElementById('task3')
-);
-
-ReactDOM.render(
-  Func(),
-  document.getElementById('task4')
 );
 
 // If you want to start measuring performance in your app, pass a function
